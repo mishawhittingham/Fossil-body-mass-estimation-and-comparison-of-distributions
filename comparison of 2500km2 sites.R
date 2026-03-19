1a78237cf0483ea5672d0e37af096301e6d60e34
mishawhittingham <157848865+mishawhittingham@users.noreply.github.com>
2026-03-19 23:51:14 +0000

    Update and rename broad 50 taph with sampling.R to comparison of 2500km2 sites.R

--- comparison of 2500km2 sites.R
+++ comparison of 2500km2 sites.R
@@ -2,17 +2,17 @@ library(moments)
 library(multimode)
 library(Matching)
 
-BM_mamms = read.csv("C:\\Wirc\\Projekt3\\Bigness\\subsets\\Broadtest50.csv", header = TRUE)
-BM_mamms20 = read.csv("C:\\Wirc\\Projekt3\\Bigness\\subsets\\Broadtest50_size20.csv", header = TRUE)
-BM_mamms40 = read.csv("C:\\Wirc\\Projekt3\\Bigness\\subsets\\Broadtest50_size40.csv", header = TRUE)
-BM_mamms60 = read.csv("C:\\Wirc\\Projekt3\\Bigness\\subsets\\Broadtest50_size60.csv", header = TRUE)
+BM_mamms = read.csv("C:\\...\\Broadtest50.csv", header = TRUE)
+BM_mamms20 = read.csv("C:\\...\\Broadtest50_size20.csv", header = TRUE)
+BM_mamms40 = read.csv("C:\\...\\Broadtest50_size40.csv", header = TRUE)
+BM_mamms60 = read.csv("C:\\...\\Broadtest50_size60.csv", header = TRUE)
 
 sites = 12
 sites20 = 11
 sites40 = 5
 sites60 = 4
 
-Foss = read.csv("C:\\Wirc\\Projekt3\\Bigness\\FossilBMs.csv", header = TRUE)
+Foss = read.csv("C:\\...\\FossilBMs.csv", header = TRUE)
 
 allsmallmodes = c()
 allsmallkurts = c()
@@ -588,9 +588,9 @@ aoutput[1:(sites60*100),12] = allbasemeds60
 
 colnames(aoutput) = toprow2
 
-write.csv(poutput, "C:\\Wirc\\Projekt3\\Bigness\\subsets\\pvals_broad50Taph_pgls.csv")
-write.csv(doutput, "C:\\Wirc\\Projekt3\\Bigness\\subsets\\dvals_broad50Taph_pgls.csv")
-write.csv(moutput, "C:\\Wirc\\Projekt3\\Bigness\\subsets\\modes_broad50Taph_pgls.csv")
-write.csv(koutput, "C:\\Wirc\\Projekt3\\Bigness\\subsets\\kurts_broad50Taph_pgls.csv")
-write.csv(soutput, "C:\\Wirc\\Projekt3\\Bigness\\subsets\\skews_broad50Taph_pgls.csv")
-write.csv(aoutput, "C:\\Wirc\\Projekt3\\Bigness\\subsets\\medians_broad50Taph_pgls.csv")
+write.csv(poutput, "C:\\...\\pvals_broad50Taph_pgls.csv")
+write.csv(doutput, "C:\\...\\dvals_broad50Taph_pgls.csv")
+write.csv(moutput, "C:\\...\\modes_broad50Taph_pgls.csv")
+write.csv(koutput, "C:\\...\\kurts_broad50Taph_pgls.csv")
+write.csv(soutput, "C:\\...\\skews_broad50Taph_pgls.csv")
+write.csv(aoutput, "C:\\...\\medians_broad50Taph_pgls.csv")
