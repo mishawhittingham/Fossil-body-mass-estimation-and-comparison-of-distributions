933532d84083acde54b933412cd10fe309ac93d1
mishawhittingham <157848865+mishawhittingham@users.noreply.github.com>
2026-08-28 17:17:11 -0400

    Update comparison of sites with only abducted-limb tetrapods.R

--- comparison of sites with only abducted-limb tetrapods.R
+++ comparison of sites with only abducted-limb tetrapods.R
@@ -2,6 +2,7 @@ library(moments)
 library(multimode)
 library(Matching)
 
+#see "testing of taphonomic models.R" for fully annotated template used as the basis for all code for comparing modern and fossil sites
 #read csv files of body masses of abducted-limb tetrapods from modern sites at various sample sizes, as well as the fossil body mass distibutions
 
 BM_mamms = read.csv("C:\\...\\TetBMs_tubesTaphready.csv", header = TRUE)
