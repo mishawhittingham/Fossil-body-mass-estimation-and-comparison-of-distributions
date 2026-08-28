75b51944317aded0dad6e67bc2db08192d0ffb52
mishawhittingham <157848865+mishawhittingham@users.noreply.github.com>
2026-08-28 17:16:48 -0400

    Update comparison of sites with only reptiles.R

--- comparison of sites with only reptiles.R
+++ comparison of sites with only reptiles.R
@@ -1,7 +1,7 @@
 library(moments)
 library(multimode)
 library(Matching)
-
+#see "testing of taphonomic models.R" for fully annotated template used as the basis for all code for comparing modern and fossil sites
 #read csv files of reptile body masses from modern sites at various sample sizes, as well as the fossil body mass distibutions
 
 BM_mamms = read.csv("C:\\...\\TetBMs_herpsTaphready.csv", header = TRUE)
