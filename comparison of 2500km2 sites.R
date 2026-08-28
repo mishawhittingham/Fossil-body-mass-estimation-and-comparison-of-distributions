e7409b1b1a7989c6df41cf0a279fd2a7cf92857c
mishawhittingham <157848865+mishawhittingham@users.noreply.github.com>
2026-08-28 17:18:06 -0400

    Update comparison of 2500km2 sites.R

--- comparison of 2500km2 sites.R
+++ comparison of 2500km2 sites.R
@@ -2,6 +2,7 @@ library(moments)
 library(multimode)
 library(Matching)
 
+#see "testing of taphonomic models.R" for fully annotated template used as the basis for all code for comparing modern and fossil sites
 #read csv files of body masses from 2500 km2 modern sites at various sample sizes, as well as the fossil body mass distibutions
 
 BM_mamms = read.csv("C:\\...\\Broadtest50.csv", header = TRUE)
